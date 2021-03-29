aa8d35029eb47aa214486d1e78271d2be999e14b
wimateusav <wimateusav@unal.edu.co>
2021-03-29 11:54:45 -0500

    My first test with this shit

---
+++ qiubit/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from qiubit.users.models import User
+from qiubit.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
